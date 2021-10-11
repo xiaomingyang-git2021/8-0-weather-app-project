85931817155bc9c674033dfffe2ee1f5fee4bb0b
Xiaoming Yang <xiaomingyang@pursuit.com>
2021-10-11 15:49:58 -0400

    updated three files (not finished yet)

---
+++ script.js
@@ -0,0 +1,71 @@
+let form = document.querySelector("form#city_choice");
+
+form.addEventListener("submit",(e)=>{
+  
+    e.preventDefault();
+    let selectedCity = e.target["city_name"].value;
+    console.log(selectedCity);
+    e.target["city_name"].value ="";
+
+    // let city = selectedCity[0].toUpperCase() + selectedCity.slice(1);
+    selectedCity = selectedCity.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+    fetch(`https://wttr.in/${selectedCity}?format=j1`)
+    .then((result)=>{
+        return result.json();
+    }).then((data)=>{
+        let main = document.querySelector("#main");
+        main.innerHTML = ` 
+        <div><h2>${selectedCity}<h2></div>
+        <div id="display-details">
+            <h2 id="selected-city"></h2>
+        </div>
+            <div id="display-area"><strong>Area:</strong> ${data.nearest_area[0].areaName[0].value}</div>
+            <div id="display-region"><strong>Region:</strong> ${data.nearest_area[0].region[0].value}</div>
+            <div id="display-country"><strong>Country:</strong> ${data.nearest_area[0].country[0].value}</div>
+            <div id="display-currently"> <strong>Currently:</strong> ${data.current_condition[0].FeelsLikeF} °F</div>`;
+        
+        let weatherResults = document.querySelector("#weather_results");
+        weatherResults.innerHTML = `
+         <div class="weather_results_today">
+                    <div>
+                        <h3>Today</h3>
+                    </div>
+                    <div>Average Tempretaure: ${data.weather[0].avgtempF} °F</div>
+                    <div>Max Temperature: ${data.weather[0].maxtempF} °F</div>
+                    <div>Min Temperature: ${data.weather[0].mintempF} °F</div>
+            </div>
+            <div class="weather_results_tomorrow">
+                    <div>
+                        <h3>Tomorrow</h3>
+                    </div>
+                    <div>Average Tempretaure: ${data.weather[1].avgtempF} °F</div>
+                    <div>Max Temperature: ${data.weather[1].maxtempF} °F</div>
+                    <div>Min Temperature: ${data.weather[1].mintempF} °F</div>
+            </div>
+            <div class="weather_results_after_tomorrow">
+                    <div>
+                        <h3>Day After Tomorrow</h3>
+                    </div>
+                    <div>Average Tempretaure: ${data.weather[2].avgtempF} °F</div>
+                    <div>Max Temperature: ${data.weather[2].avgtempF} °F</div>
+                    <div>Min Temperature: ${data.weather[2].avgtempF} °F</div>
+            </div>`;
+            let message = document.querySelector("#message");
+            let ul = document.querySelector("#previous_searches");
+            let li = document.createElement("li");
+            let a = document.createElement("a");
+            a.href = "#";
+            a.textContent = selectedCity;
+            li.append(a);
+            ul.append(li);
+            if(message){
+                message.remove();
+            }
+
+            
+
+                
+    }).catch((error)=>{
+        console.log(error)
+    });
+});
