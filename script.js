7722ff03b961b90ab3dda5c1f0d8a2f49660b464
Xiaoming Yang <86433186+xiaomingyang-git2021@users.noreply.github.com>
2021-10-24 16:11:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -46,17 +46,17 @@ function weatherForcast(cityName){
                 <div>
                     <h3>Tomorrow</h3>
                 </div>
-                <div>Average Tempretaure: ${data.weather[1].avgtempF} °F</div>
-                <div>Max Temperature: ${data.weather[1].maxtempF} °F</div>
-                <div>Min Temperature: ${data.weather[1].mintempF} °F</div>
+                <div>Average Tempretaure: ${data.weather[1].avgtempF} °F / ${data.weather[1].avgtempC} °C</div>
+                <div>Max Temperature: ${data.weather[1].maxtempF} °F / ${data.weather[1].maxtempC} °C</div>
+                <div>Min Temperature: ${data.weather[1].mintempF} °F / ${data.weather[1].mintempC} °C</div>
         </div>
         <div id="weather_results_after_tomorrow">
                 <div>
                     <h3>Day After Tomorrow</h3>
                 </div>
-                <div>Average Tempretaure: ${data.weather[2].avgtempF} °F</div>
-                <div>Max Temperature: ${data.weather[2].maxtempF} °F</div>
-                <div>Min Temperature: ${data.weather[2].mintempF} °F</div>
+                <div>Average Tempretaure: ${data.weather[2].avgtempF} °F / ${data.weather[2].avgtempC} °C</div>
+                <div>Max Temperature: ${data.weather[2].maxtempF} °F / ${data.weather[2].maxtempC} °C</div>
+                <div>Min Temperature: ${data.weather[2].mintempF} °F / ${data.weather[2].mintempC} °C</div>
         </div>`;
         let message = document.querySelector("#message");
         if(message){
@@ -67,7 +67,7 @@ function weatherForcast(cityName){
         let a = document.createElement("a");
         let span = document.createElement("span");
         a.href = "#";
-        span.textContent = ` - ${data.current_condition[0].FeelsLikeF}°F `;
+        span.textContent = ` - ${data.current_condition[0].FeelsLikeF}°F / ${data.current_condition[0].FeelsLikeC}°C `;
         a.textContent = `${cityName}`;
 
         
